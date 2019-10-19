d7310276fb41f11fd351e07939d22b6436969e39
Anton Cheng <anton@coolbitx.com>
2019-10-19 21:43:36 +0800

    Add verify badges

--- src/Components/MatchCard.js
+++ src/Components/MatchCard.js
@@ -3,9 +3,12 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import Badge from 'react-bootstrap/Badge'
-import VerificationModal from './VerifyModal'
+import VerificationBadge from './VerifyModal'
 import Web3 from 'web3'
 import abi from '../abi'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const web3 = new Web3(Web3.givenProvider)
 export default class Match extends Component {
@@ -18,12 +21,15 @@ export default class Match extends Component {
 
   async checkMatchStatus() {
     const contract = new web3.eth.Contract(abi, this.props.match.address)
-    const homeVerified = await contract.methods.awayTeamVerified().call()
-    const awayVerified = await contract.methods.homeTeamVerified().call()
+    const homeVerified = await contract.methods.homeTeamVerified().call()
+    const awayVerified = await contract.methods.awayTeamVerified().call()
     const home = await contract.methods.homeTeam().call()
     const away = await contract.methods.awayTeam().call()
     const verified = homeVerified && awayVerified
-    this.setState({ home, away, verified, homeVerified, awayVerified, contract })
+
+    const token = await contract.methods.betToken().call();
+
+    this.setState({ home, away, verified, homeVerified, awayVerified, contract, token })
   }
 
   render() {
@@ -31,26 +37,40 @@ export default class Match extends Component {
     const badges = this.state.verified ? (
       <Badge variant='success'> Verified </Badge>
     ) : (
-      <VerificationModal
+      <VerificationBadge
         account={this.props.account}
         contract={this.state.contract}
         homeVerified={this.state.homeVerified}
         awayVerified={this.state.awayVerified}
       />
     )
-    // : <Badge variant='dark'> Not verified </Badge>
 
     return (
       <Card style={{ width: 'max', margin: 10 }}>
         <Card.Body>
           <Card.Title>
-            {this.state.home} vs {this.state.away}
+            <Container>
+              <Row>
+                <Col xs>{this.state.home}</Col>
+                <Col xs style={{ textAlign: 'center' }}>
+                  vs
+                </Col>
+                <Col>{this.state.away}</Col>
+              </Row>
+            </Container>
           </Card.Title>
           <Card.Subtitle className='mb-2 text-muted'>
-            {this.props.match.address} {badges}
+            <Container>
+              {this.props.match.address} {badges}
+            </Container>
           </Card.Subtitle>
-          <Card.Text>{this.props.match.time}</Card.Text>
-          <Button variant='primary'>Go Bet</Button>
+          <Card.Text>
+            <Container>
+            {/* {this.props.match.time} */}
+            <Button variant='primary'>Go Bet</Button>
+            </Container>
+          </Card.Text>
+          
         </Card.Body>
       </Card>
     )
