c096bea1558d8ffae88dd35c162a24306dfb1750
Anton Cheng <anton@coolbitx.com>
2019-10-19 21:44:38 +0800

    Update verify modals

--- src/Components/VerifyModal.js
+++ src/Components/VerifyModal.js
@@ -6,42 +6,40 @@ import Badge from 'react-bootstrap/Badge'
 export default class VerificationBadge extends Component {
   state = {
     show: false,
-    progress: 0
+    progress: 0,
   }
   render() {
     let homeVerifyButton = this.props.homeVerified ? (
-      <Button
-        variant='outline-secondary'
+        <Button variant='outline-secondary' disabled>
+          Verify Home Team
+        </Button>
+      ):
+      (
+        <Button
+        variant='secondary'
         onClick={() => {
           this.props.contract.methods.verifyHomeTeam().send({ from: this.props.account })
         }}
       >
-        {' '}
-        Verify Home Team{' '}
+        Verify Home Team
       </Button>
-    ) : (
-      <Button variant='outline-secondary' disabled>
-        {' '}
-        Verify Home Team{' '}
-      </Button>
-    )
+    ) 
 
     let awayVerifyButton = this.props.awayVerified ? (
-        <Button
-          variant='outline-secondary'
-          onClick={() => {
-            this.props.contract.methods.verifyAwayTeam().send({ from: this.props.account })
-          }}
-        >
-          {' '}
-          Verify Away Team{' '}
-        </Button>
-      ) : (
         <Button variant='outline-secondary' disabled>
-          {' '}
-          Verify Away Team{' '}
+          Verify Away Team
         </Button>
       )
+      : (
+      <Button
+        variant='secondary'
+        onClick={() => {
+          this.props.contract.methods.verifyAwayTeam().send({ from: this.props.account })
+        }}
+      >
+        Verify Away Team
+      </Button>
+    )
 
     return (
       <>
